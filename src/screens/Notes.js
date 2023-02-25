4035f0075e9c77dba68cd233b4641228c7f3f7ac
Ndyabagye Henry <ndyabagyehenrytusi@gmail.com>
2023-02-25 15:32:30 +0300

    Adding navigation for screens

---
+++ src/screens/Notes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Notes = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>I am the notes screen </Text>
+    </View>
+  );
+};
+
+export default Notes;
